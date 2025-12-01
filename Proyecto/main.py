85ec3e657bf44bcd8a9230f658be2852c35ad1c8
NormaSeleneSanchez <selene.sanchez@ciencias.unam.mx>
2025-11-30 22:18:26 -0600

    Nuevo main

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,1 +1,24 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, TabbedContent, TabPane, RichLog
+from contextlib import redirect_stdout
+import io
+import psutil
 
+
+# Asi podremos usar lo que ya funcionaba en terminal
+from Monitos import (
+    show_system_info,
+    show_cpu,
+    show_memory,
+    show_disk,
+    show_network,
+    show_temperatures,
+    show_processes
+)
+
+class Monitor(App):
+
+    
+
+if __name__ == "__main__":
+    
